76dd98399d900119a746e7db714f30c3b6e7148c
arcnmx <arcnmx@users.noreply.github.com>
2020-07-09 20:37:57 -0700

    tokio update

--- src/tokio.rs
+++ src/tokio.rs
@@ -1,11 +1,11 @@
 #![allow(missing_docs)]
 
-extern crate tokio_io;
+extern crate tokio_util;
 extern crate bytes;
 
 use std::{io, mem, ptr, slice};
 use sys::input_event;
-use self::tokio_io::codec::{Decoder, Encoder};
+use self::tokio_util::codec::{Decoder, Encoder};
 use self::bytes::{BytesMut, BufMut};
 use InputEvent;
 
@@ -41,14 +41,13 @@ impl Decoder for EventCodec {
     }
 }
 
-impl Encoder for EventCodec {
-    type Item = InputEvent;
+impl Encoder<InputEvent> for EventCodec {
     type Error = io::Error;
 
-    fn encode(&mut self, item: Self::Item, dst: &mut BytesMut) -> Result<(), Self::Error> {
-        dst.reserve(mem::size_of::<Self::Item>());
+    fn encode(&mut self, item: InputEvent, dst: &mut BytesMut) -> Result<(), Self::Error> {
+        dst.reserve(mem::size_of::<InputEvent>());
         unsafe {
-            dst.put_slice(slice::from_raw_parts(&item as *const _ as *const u8, mem::size_of::<Self::Item>()));
+            dst.put_slice(slice::from_raw_parts(&item as *const _ as *const u8, mem::size_of::<InputEvent>()));
         }
         Ok(())
     }
